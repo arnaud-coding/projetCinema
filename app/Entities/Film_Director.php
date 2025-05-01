7e5b722c9508c43ebeb9f637104f03de51502e07
arnaud-coding <arnaudberthollet@hotmail.fr>
2025-05-01 20:57:28 +0200

    entities - done

---
+++ app/Entities/Film_Director.php
@@ -0,0 +1,35 @@
+<?php
+// ---------------------------------------
+// CLASSE DE L'ENTITE film_director DE LA BDD
+//
+// (Table de jointure entre film et director)
+// ---------------------------------------
+class Film_Director
+{
+    private $id_film;
+    private $id_director;
+
+    // -------------------
+    //  GETTERS ET SETTERS
+    // -------------------
+
+    public function getId_film()
+    {
+        return $this->id_film;
+    }
+    public function setId_film($id_film)
+    {
+        $this->id_film = $id_film;
+        return $this;
+    }
+
+    public function getId_director()
+    {
+        return $this->id_director;
+    }
+    public function setId_director($id_director)
+    {
+        $this->id_director = $id_director;
+        return $this;
+    }
+}
---
+++ app/Entities/Film_Genre.php
@@ -0,0 +1,36 @@
+<?php
+// ---------------------------------------
+// CLASSE DE L'ENTITE film_genre DE LA BDD
+//
+// (Table de jointure entre film et genre)
+// ---------------------------------------
+class Film_Genre
+{
+    private $id_film;
+    private $id_genre;
+
+    // -------------------
+    //  GETTERS ET SETTERS
+    // -------------------
+
+    public function getId_film()
+    {
+        return $this->id_film;
+    }
+    public function setId_film($id_film)
+    {
+        $this->id_film = $id_film;
+        return $this;
+    }
+
+
+    public function getId_genre()
+    {
+        return $this->id_genre;
+    }
+    public function setId_genre($id_genre)
+    {
+        $this->id_genre = $id_genre;
+        return $this;
+    }
+}
